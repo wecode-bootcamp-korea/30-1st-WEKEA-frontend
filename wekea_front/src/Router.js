1d434eab58dd70798a8c6e5e4c07588d1ffc3dfb
안광민 <kwangmon@angwangmin-ui-MacBookPro.local>
2022-02-28 18:21:58 +0900

    ADD:프론트 초기설정

---
+++ wekea_front/src/Router.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+// import Main from './pages/Main/Main';
+// import Signup from './pages/Signup/Signup';
+// import Login from './pages/Login/Login';
+// import ItemList from './pages/ItemList/ItemList';
+// import ItemDetail from './pages/ItemDetail/ItemDetail';
+
+const Router = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        {/* <Route path="/" element={<Main />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/itemlist" element={<ItemList />} />
+        <Route path="/itemdetail" element={<ItemDetail />} /> */}
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default Router;
