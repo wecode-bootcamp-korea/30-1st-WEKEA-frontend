fceac90492de0e8972f9fc00c00f45cd1174fd41
안광민 <kwangmon@angwangmin-ui-MacBookPro.local>
2022-03-04 19:08:49 +0900

    FIX:login 페이지 피드백 1차 반영

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,9 +1,20 @@
-import './Login.scss';
+import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { useState, useEffect } from 'react';
+import './Login.scss';
 
 export default function Login() {
   const navigate = useNavigate();
+
+  const [userId, setUserId] = useState('');
+  const [userPassword, SetUserPassword] = useState('');
+
+  const handleIdInput = e => {
+    setUserId(e.target.value);
+  };
+
+  const handlePasswordInput = e => {
+    SetUserPassword(e.target.value);
+  };
 
   const goToSignUp = () => {
     navigate('/signup');
@@ -11,43 +22,62 @@ export default function Login() {
 
   const handleLogin = e => {
     e.preventDefault();
-    goToSignUp();
+    fetch('http://10.58.2.75:8000/users/login', {
+      method: 'POST',
+      body: JSON.stringify({
+        email: userId,
+        password: userPassword,
+      }),
+    })
+      .then(res => res.json())
+      .then(result => console.log('결과: ', result));
   };
+
+  const emailRegExp =
+    /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/i;
+  const passwordRegExp =
+    /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[$@$!%*?&])[A-Za-z\d$@$!%*?&]{8,}/;
+
   return (
-    <div className="loginBox">
-      <div className="loginLeft">
-        <div className="loginLeftNav">
-          <div className="loginLeftNav">icon</div>
-          <div className="loginLeftNav">icon</div>
+    <div className="login">
+      <div className="backBlue">
+        <div className="backBlueNav">
+          <div className="backBlueNav">icon</div>
+          <div className="backBlueNav">icon</div>
         </div>
-        <div className="loginLeftMain">
+        <div className="backBlueMain">
           <h1>로그인</h1>
           <p>가나다라마바사아자차카타파하abcdefgfijklmnopqrstuvwxyz</p>
         </div>
-        <div className="loginLeftFooter">
+        <div className="backBlueFooter">
           <span>
             IKEHEYO.kr - 쿠키 정책 , 개인정보처리방침 <br />© WECODE IKEHEYO
             Systems 2022-2222
           </span>
         </div>
       </div>
-      <div className="loginRight">
+      <div className="backWhite">
         <form className="loginGate" onSubmit={handleLogin}>
           <div className="loginInput">
-            <span>이메일</span>
-            <input id="loginId" type="text" />
+            <span className={userId.length > 0 && 'spanFix'}>이메일</span>
+            <input id="loginId" value={userId} onChange={handleIdInput} />
           </div>
           <div className="loginInput">
-            <span>비밀번호</span>
-            <input id="loginPassword" type="password" />
+            <span className={userPassword.length > 0 && 'spanFix'}>
+              비밀번호
+            </span>
+            <input
+              id="loginPassword"
+              type="password"
+              value={userPassword}
+              onChange={handlePasswordInput}
+            />
           </div>
-          <div className="loginBtn">
-            <button>로그인</button>
-          </div>
+          <button type="submit">로그인</button>
           <p>IKEHEYO 계정이 없으신가요? 지금 바로 만들어보세요</p>
-          <div className="signUPBtn">
-            <button onClick={goToSignUp}>회원가입</button>
-          </div>
+          <button className="signUpBtn" type="button" onClick={goToSignUp}>
+            회원가입
+          </button>
         </form>
       </div>
     </div>
